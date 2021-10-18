1f5a124d40a1855b3ec68e7c5c07978c93ff8bbe
NGUYỄN HỬU TRÍ <nguyenhuutri31081999nht@gmail.com>
2021-10-18 17:33:24 +0700

    init

--- src/views/components/Content/index.js
+++ src/views/components/Content/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./style.scss";
+import MyCarousel from "./MyCarousel";
 
 export default function index() {
     return (
         <div id="content" className="col d-none d-md-block">
-            {/* <Carousel /> */}
+            <MyCarousel />
         </div>
     );
 }
