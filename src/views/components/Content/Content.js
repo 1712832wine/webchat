d8d39d44b3d7d208235ea7be73cb3b0ebf18b8e6
NGUYỄN HỬU TRÍ <nguyenhuutri31081999nht@gmail.com>
2021-11-26 19:12:13 +0700

    fix carousel

--- src/views/components/Content/Content.js
+++ src/views/components/Content/Content.js
@@ -3,6 +3,7 @@ import "./Content.scss";
 
 import { Switch, Route } from "react-router-dom";
 import routers from "../../../routersChatbot";
+import { Carousel } from "react-bootstrap";
 
 export default function Content() {
     return (
@@ -16,7 +17,52 @@ export default function Content() {
                     );
                 })}
                 <Route path="/">
-                    <h1>This is carousel</h1>
+                    <Carousel>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://chat.zalo.me/assets/quick-message-onboard.3950179c175f636e91e3169b65d1b3e2.png"
+                                alt="First slide"
+                            />
+                            <Carousel.Caption>
+                                <h3>First slide label</h3>
+                                <p>
+                                    Nulla vitae elit libero, a pharetra augue
+                                    mollis interdum.
+                                </p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://chat.zalo.me/assets/inapp-welcome-screen-04.ade93b965a968b16f2203e9d63b283a7.jpg"
+                                alt="Second slide"
+                            />
+
+                            <Carousel.Caption>
+                                <h3>Second slide label</h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit.
+                                </p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://chat.zalo.me/assets/inapp-welcome-screen-01.469ad7daf26e0303dd0d54eb5262e795.jpg"
+                                alt="Third slide"
+                            />
+
+                            <Carousel.Caption>
+                                <h3>Third slide label</h3>
+                                <p>
+                                    Praesent commodo cursus magna, vel
+                                    scelerisque nisl consectetur.
+                                </p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
                 </Route>
             </Switch>
         </div>
