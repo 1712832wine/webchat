56050c1c5d4fe6615755e09160b136d72d61e42e
NGUYỄN HỬU TRÍ <nguyenhuutri31081999nht@gmail.com>
2021-11-28 19:43:57 +0700

    fix step 1

--- src/views/components/Content/Content.js
+++ src/views/components/Content/Content.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Content.scss";
 
-import { Carousel } from "react-bootstrap";
+import { Carousel, Image } from "react-bootstrap";
 import { connect } from "react-redux";
 import MessageBox from "./MessageBox/MessageBox";
 
@@ -11,50 +11,27 @@ function Content({ id }) {
             {id ? (
                 <MessageBox />
             ) : (
-                <Carousel>
+                <Carousel variant="dark">
                     <Carousel.Item>
-                        <img
+                        <Image
                             className="d-block w-100"
                             src="https://chat.zalo.me/assets/quick-message-onboard.3950179c175f636e91e3169b65d1b3e2.png"
                             alt="First slide"
                         />
-                        <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>
-                                Nulla vitae elit libero, a pharetra augue mollis
-                                interdum.
-                            </p>
-                        </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img
+                        <Image
                             className="d-block w-100"
                             src="https://chat.zalo.me/assets/inapp-welcome-screen-04.ade93b965a968b16f2203e9d63b283a7.jpg"
                             alt="Second slide"
                         />
-
-                        <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit.
-                            </p>
-                        </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img
+                        <Image
                             className="d-block w-100"
                             src="https://chat.zalo.me/assets/inapp-welcome-screen-01.469ad7daf26e0303dd0d54eb5262e795.jpg"
                             alt="Third slide"
                         />
-
-                        <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                            <p>
-                                Praesent commodo cursus magna, vel scelerisque
-                                nisl consectetur.
-                            </p>
-                        </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
             )}
