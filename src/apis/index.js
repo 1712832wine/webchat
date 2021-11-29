67e4342b97bf779f1fe864c83db33fa2160f0d8a
NGUYỄN HỬU TRÍ <nguyenhuutri31081999nht@gmail.com>
2021-11-29 15:36:03 +0700

    ok

--- src/apis/index.js
+++ src/apis/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apis = {
     send_message: (data) => {
-        return axios.post("http://localhost:8080/api/messages/send", data);
+        return axios.post("http://localhost:5000/api/messages/send", data);
     },
 };
 export default apis;
